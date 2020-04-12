2edabed0d0dd84fd302bbe3d96675964466bd6e5
qypec <qypec.prog@yandex.ru>
2020-04-12 17:34:31 +0300

    scheduler_round_robin| added structure of program, initial functions

--- scheduler_round_robin/header.h
+++ scheduler_round_robin/header.h
@@ -1,9 +1,46 @@
 #ifndef HEADER_H
 # define HEADER_H
 
-typedef struct		s_scheduler
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct			s_list
 {
-	
-}					t_scheduler;
+	void				*content;
+	size_t				content_size;
+	struct s_list		*next;
+}						t_list;
+
+typedef struct			s_thread
+{
+	int					id;
+}						t_thread;
+
+typedef struct			s_scheduler
+{
+	t_list				*thread_queue;
+	t_thread			*running_thread;
+	t_list				*blocked_thread;
+	int					timeslice;
+	int					running_time_thread;
+}						t_scheduler;
+
+t_scheduler				*g_scheduler;
+
+t_scheduler				*init_scheduler(int timeslice);
+void					delete_scheduler(t_scheduler **s);
+t_thread				*create_thread(int thread_id);
+void					delete_thread(t_thread **t);
+
+void					del_content_thread(void *content, size_t content_size);
+
+t_list					*ft_lstnew(void *content, size_t content_size);
+void					ft_lstpushback(t_list **alst, t_list *new);
+void					ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void					ft_lstdelone(t_list **alst, \
+							void (*del)(void *, size_t));
+void					ft_lstdelhead(t_list **alst, \
+							void (*del)(void *, size_t));
+t_list					*ft_extracthead(t_list *lst);
 
 #endif
