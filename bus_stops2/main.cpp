eae3591232b399384e619815834fc03e9bb4de1d
qypec <qypec.prog@yandex.ru>
2020-05-05 14:59:10 +0300

    added bus_stops2

---
+++ bus_stops2/main.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int                                 main()
+{
+    int                             Q, n, number_counter;
+    map<vector<string>, int>        route;
+    vector<string>                  stop_list;
+    string                          stop;
+
+    cin >> Q;
+    number_counter = 1;ls
+    for (auto i = 0; i < Q; i++)
+    {
+        stop_list.clear();
+        cin >> n;
+        for (auto j = 0; j < n; j++)
+        {
+            cin >> stop;
+            stop_list.push_back(stop);
+        }
+        if (route.count(stop_list) != 0)
+            cout << "Already exists for " << route[stop_list] << endl;
+        else
+        {
+            route[stop_list] = number_counter++;
+            cout << "New bus " << route[stop_list] << endl;
+        }
+    }
+}
