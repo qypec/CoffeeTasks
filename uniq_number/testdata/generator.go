c034351d47844fb75b096be6287f0fd796e0f616
qypec <qypec.prog@yandex.ru>
2020-05-16 18:30:47 +0300

    uniq_number | refactored testdata generator

--- uniq_number/testdata/generator.go
+++ uniq_number/testdata/generator.go
@@ -1,24 +1,24 @@
-package testdata
+package main
 
 import (
-	"os"
 	"fmt"
+	"os"
 )
 
 const limit = 1000000
 
 func main() {
-	file, err := os.Create("testdata.txt")
-     
-    if err != nil{
-        fmt.Println("Unable to create file:", err) 
-        os.Exit(1) 
-    }
-    defer file.Close() 
+	file, err := os.Create("1_000_000_test.txt")
+
+	if err != nil {
+		fmt.Println("Unable to create file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
 	for i := 1; i <= limit; i++ {
-		fmt.Fprintf(file, "%v %v ", i, i);
-		fmt.Println(i)
+		fmt.Fprintf(file, "%v %v ", i, i)
+		// fmt.Println(i)
 	}
-	fmt.Fprintf(file, "0");
+	fmt.Fprintf(file, "0")
 }
