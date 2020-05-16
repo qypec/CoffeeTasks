25e7cf85faf5c7feaa819e801dfdf0b6bdf0b35d
qypec <qypec.prog@yandex.ru>
2020-05-16 21:42:39 +0300

    merge_channels | added unbuffered test to blocking test

--- merge_channels/merge_channels.go
+++ merge_channels/merge_channels.go
@@ -9,13 +9,14 @@ func executeWorker(f func(int) int, x1 int, res chan<- int) {
 func Merge2Channels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
 	go func() {
 		for i := 0; i < n; i++ {
+			// fmt.Println("Merge2Channels ", i)
 			x1 := <-in1
 			x2 := <-in2
-			
+
 			res := make(chan int, 2)
 			go executeWorker(f, x1, res)
 			go executeWorker(f, x2, res)
-			
+
 			sum := 0
 			for j := 0; j < 2; j++ {
 				sum += <-res
--- merge_channels/merge_channels_test.go
+++ merge_channels/merge_channels_test.go
@@ -1,43 +1,66 @@
 package main
 
 import (
-
-	"testing"
 	"github.com/stretchr/testify/require"
+	"testing"
 	"time"
 	// "sync"
 )
 
-func blockingWorker(x int) int {
-	time.Sleep(1 * time.Millisecond)
+func workerMillisecond(x int) int {
+	time.Sleep(time.Millisecond)
 	return x * 10
 }
 
-const blockingTestNumbers = 1000
+func workerSecond(x int) int {
+	time.Sleep(time.Second)
+	return x * 10
+}
 
-// tests Merge2Channels for non-blocking
+// tests Merge2Channels for non-blocking (buffered / unbuffered channels)
 func TestMerge2ChannelsBlocking(t *testing.T) {
-	ch1 := make(chan int, blockingTestNumbers)
-	ch2 := make(chan int, blockingTestNumbers)
-	out := make(chan int, blockingTestNumbers)
-	defer close(ch1)
-	defer close(ch2)
+	testsNumber := 1000
+
+	in1 := make(chan int, testsNumber)
+	in2 := make(chan int, testsNumber)
+	out := make(chan int, testsNumber)
+	defer close(in1)
+	defer close(in2)
 	defer close(out)
 
-	for i := 0; i < blockingTestNumbers; i++ {
-		ch1 <- i
-		ch2 <- i
+	for i := 0; i < testsNumber; i++ {
+		in1 <- i
+		in2 <- i
 	}
 
 	start := time.Now()
-	Merge2Channels(blockingWorker, ch1, ch2, out, blockingTestNumbers)
+	Merge2Channels(workerMillisecond, in1, in2, out, testsNumber)
 	end := int64(time.Since(start))
 
 	expectedTime := int64(time.Millisecond)
 	require.GreaterOrEqual(t, expectedTime, end, "blocking Merge2Channels")
 
 	// waiting for the completion goroutines to avoid datarace
-	for i := 0; i < blockingTestNumbers; i++ {
+	for i := 0; i < testsNumber; i++ {
+		res := <-out
+		require.Equal(t, (i*10 + i*10), res)
+	}
+
+	// unbuffered cahnnels
+	in1 = make(chan int)
+	in2 = make(chan int)
+	out = make(chan int)
+
+	start = time.Now()
+	Merge2Channels(workerMillisecond, in1, in2, out, testsNumber)
+	end = int64(time.Since(start))
+
+	expectedTime = int64(time.Millisecond)
+	require.GreaterOrEqual(t, expectedTime, end, "blocking Merge2Channels unbuffered")
+
+	for i := 0; i < testsNumber; i++ {
+		in1 <- i
+		in2 <- i
 		_ = <-out
 	}
 }
