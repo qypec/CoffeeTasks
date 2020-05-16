754bb3f4d03ed4005c2a3aeb085ba431dc41f3e1
qypec <qypec.prog@yandex.ru>
2020-05-16 18:31:15 +0300

    uniq_number | refactored, added new tests

--- uniq_number/main.go
+++ uniq_number/main.go
@@ -8,14 +8,16 @@ import (
 	"strconv"
 )
 
+// Uniq finds a unique integer among couples
+// tests -> github.com/qypec/coffee-tasks/tree/master/uniq_number
 func Uniq(in io.Reader) int {
-	scanner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in) // MaxScanTokenSize = 64 * 1024
 	scanner.Split(bufio.ScanWords)
 
 	sum := 0
 	for scanner.Scan() {
 		num, _ := strconv.Atoi(scanner.Text())
-		sum ^= num;
+		sum ^= num
 	}
 	return sum
 }
--- uniq_number/main_test.go
+++ uniq_number/main_test.go
@@ -1,17 +1,66 @@
 package main
 
 import (
-	"testing"
+	"bytes"
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os"
-	"bytes"
+	"strings"
+	"testing"
 )
+
+const tenMinllionTestPath = "./testdata/10_000_000_test.txt"
 
 func TestUniq(t *testing.T) {
 
-// Test 01 | 1.000.000 pairs
-	file, err := os.Open("./testdata/1_000_000_test.txt")
+	// Test 01 | 1.000.000 pairs
+	{
+		file, err := os.Open(tenMinllionTestPath)
+		if err != nil {
+			panic(err)
+		}
+
+		fileContent, err := ioutil.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+		file.Close()
+
+		require.Equal(t, 0, Uniq(bytes.NewReader(fileContent)))
+	}
+
+	testCases := []string{
+		/* 01 */ "1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1", // basic
+		/* 02 */ "11111111111 11111111111 898899899889 898899899889 33333333", // big numbers
+		/* 03 */ "11 22 33 33 33 22 11", // more then two equal numbers
+		/* 04 */ "1 11 22 33 33 33 11 33 22 22 33 22 33 1 1 1 1 22 22",
+		/* 05 */ "0 0 0 0 0 0 0 0 0 0 0 0 0", // zero
+		/* 06 */ "-100 -500 100 100 -500", // negative
+		/* 07 */ "-100 -0 +0 -100 +0",
+		/* 08 */ "-100 -0 +0 -100 -0",
+		/* 08 */ "-100 -0 +0 -100 -0 0 +0",
+	}
+
+	testExpected := []interface{}{
+		/* 01 */ 9,
+		/* 02 */ 33333333,
+		/* 03 */ 33,
+		/* 04 */ 1,
+		/* 05 */ 0,
+		/* 06 */ -100,
+		/* 07 */ 0,
+		/* 08 */ 0,
+		/* 09 */ 0,
+	}
+
+	for i, testCase := range testCases {
+		require.Equal(t, testExpected[i], Uniq(strings.NewReader(testCase)))
+	}
+}
+
+// go test -bench . -benchmem
+func BenchmarkFast(b *testing.B) {
+	file, err := os.Open(tenMinllionTestPath)
 	if err != nil {
 		panic(err)
 	}
@@ -22,5 +71,8 @@ func TestUniq(t *testing.T) {
 	}
 	file.Close()
 
-	require.Equal(t, 0, Uniq(bytes.NewReader(fileContent)))
+	fileContentReader := bytes.NewReader(fileContent)
+	for i := 0; i < b.N; i++ {
+		Uniq(fileContentReader)
+	}
 }
