08ea808b069a81050262d92b571dff7470c05bca
qypec <qypec.prog@yandex.ru>
2020-05-21 14:39:03 +0300

    seq_sum | some optimizing

--- seq_sum/main.go
+++ seq_sum/main.go
@@ -10,70 +10,39 @@ import (
 const inputFileName = "input.txt"
 const outputFileName = "output.txt"
 
-// my implementation of caching binary search
-type cacheT struct {
-	toFind int
-	m      int
-}
-
-// caching remembers values until the next binarySearch call
-func (prev *cacheT) caching(toFind, m int) {
-	prev.toFind = toFind
-	prev.m = m
-}
-
-// prev.m - индекс, на котором завершился binarySearch в прошлый раз
-// toFind (следующее по очереди число) <= prev.toFind, т.к. toFind = target - num (см. функцию SeqSum), a num увеличивается с каждой итерацией
-// отсюда следует, что toFind может находится в пределах одной ячейки массива от prev.m
-// эти условия и проверяет checkCache
-func (prev *cacheT) checkCache(a []int, toFind, l, r int) (bool, int) {
-	if prev.m < l || prev.m > r {
-		return false, -1
-	} // prev.m out of range
-	if prev.toFind == toFind {
-		return true, -1
-	} // If the same `toFind`, then the last time it was not found
-	if a[prev.m] == toFind {
-		return true, prev.m
-	}
-	if a[prev.m-1] == toFind {
-		return true, prev.m - 1
-	}
-	if a[prev.m-1] < toFind && a[prev.m] > toFind {
-		return true, -1
-	}
-	return false, -1
-}
-
-func ascendingMoves(a []int, m int, toFind int, fix, l, r *int) {
-	if a[m] >= toFind {
-		if a[m] == toFind {
-			*fix = m
+/* finds the first element that is larger or equal than the given(toFind) */
+/* array a must be sorted */
+/* log(n) */
+func upperBound(a []int, toFind int, l, r int) int {
+	fix := -1
+	for l <= r {
+		m := l + int((r-l)/2)
+		if a[m] >= toFind {
+			fix = m
+			r = m - 1
+		} else {
+			l = m + 1
 		}
-		*r = m - 1
-	} else {
-		*l = m + 1
 	}
+	return fix
 }
 
-/* BinarySearch finds the element that is equal than the given (toFind) */
+/* BinarySearch finds the first element that is equal than the given (toFind) */
 /* array a must be sorted */
 /* Complexity: log(n) */
-func binarySearch(a []int, toFind int, l, r int, prev cacheT) int {
-	if use, fix := prev.checkCache(a, toFind, l, r); use {
-		return fix
-	}
-
+func binarySearch(a []int, toFind int, l, r int) int {
 	fix := -1
-	m := 0
 	for l <= r {
-		m = l + int((r-l)/2)
-		ascendingMoves(a, m, toFind, &fix, &l, &r)
-		if fix != -1 {
-			break
+		m := l + int((r-l)/2)
+		if a[m] >= toFind {
+			if a[m] == toFind {
+				fix = m
+			}
+			r = m - 1
+		} else {
+			l = m + 1
 		}
 	}
-	prev.caching(toFind, m)
 	return fix
 }
 
@@ -91,7 +60,9 @@ func partition(arr []int, l, r int) int {
 	return j
 }
 
-func recQuickSort(arr []int, l, r int) {
+// quickSort sorts an array
+// Complexity: nlog(n)
+func quickSort(arr []int, l, r int) {
 	if l < 0 || r < 0 {
 		return
 	}
@@ -99,30 +70,24 @@ func recQuickSort(arr []int, l, r int) {
 		return
 	}
 	m := partition(arr, l, r)
-	recQuickSort(arr, l, m-1)
-	recQuickSort(arr, m+1, r)
+	quickSort(arr, l, m-1)
+	quickSort(arr, m+1, r)
 }
-
-// QuickSort sorts an array
-// Complexity: nlog(n)
-func quickSort(arr []int) { recQuickSort(arr, 0, len(arr)-1) }
 
 // SeqSum searches in a seq for two numbers that in total give a target.
 // If successful, it will return 1, otherwise it will return 0
 // Complexity: nlog(n)
 // tests -> github.com/qypec/coffee-tasks/tree/master/seq_sum
 func SeqSum(target int, seq []int) int {
-	quickSort(seq)
-	cache := cacheT{-1, -1}
+	quickSort(seq, 0, len(seq)-1)
 
-	// Searches for a number equal to `target - num` in a sequence
-	// nlog(n)
-	for i, num := range seq {
-		if num > target/2 {
-			break
-		}
-		fix := binarySearch(seq, target-num, i+1, len(seq)-1, cache)
-		if fix != -1 && fix != i {
+	// Removes small numbers
+	i := upperBound(seq, target-seq[len(seq)-1], 0, len(seq)-1)
+
+	// Searches for a number equal to `target - seq[i]` in a sequence
+	for ; i >= 0 && i < len(seq); i++ {
+		fix := binarySearch(seq, target-seq[i], i+1, len(seq)-1)
+		if fix != -1 {
 			return 1
 		}
 	}
--- seq_sum/main_test.go
+++ seq_sum/main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/stretchr/testify/require"
+	// "fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type seqSumTest struct {
@@ -12,20 +14,20 @@ type seqSumTest struct {
 
 func TestSeqSum(t *testing.T) {
 	testCases := []seqSumTest{
-		/* 1 */ {5, []int{1, 7, 3, 4, 7, 9}},
+		/* 1 */ {5, []int{1, 3, 4}},
 		/* 2 */ {1, []int{1}},
 		/* 3 */ {999999998, []int{999999997, 1}},
 		/* 4 */ {2, []int{1, 1, 1, 1, 1, 1, 1}},
 		/* 5 */ {3, []int{1, 1, 1, 1, 1, 1, 1}},
-		/* 6 */ {0, []int{9, 7, 10, 0, 5, 5, 0}},
-		/* 7 */ {1, []int{9, 7, 1, 2, 5, 5, 2, 0, 3, 5}},
-		/* 8 */ {5, []int{9, 7, 1, 2, 5, 6, 2, 0, 3, 2, 4, 3, 3, 1, 0, 5, 5}},
+		/* 6 */ {0, []int{0, 0}},
+		/* 7 */ {1, []int{1, 0}},
+		/* 8 */ {5, []int{1, 2, 5, 6, 2, 0, 3, 2, 4, 3, 3, 1, 0, 5, 5}},
 		/* 9 */ {99, []int{9, 7, 1, 2, 5, 6, 2, 0, 3, 2, 4, 3, 3, 1, 0, 5, 5}},
 		/* 10 */ {0, []int{0}},
 		/* 11 */ {0, []int{0, 0}},
 		/* 12 */ {0, []int{0, 0, 0}},
-		/* 13 */ {22, []int{1, 3, 4, 12, 23, 24, 6, 3, 7, 10}},
-		/* 14 */ {6, []int{1, 7, 3, 4, 7, 9}},
+		/* 13 */ {22, []int{1, 3, 4, 12, 6, 3, 7, 10}},
+		/* 14 */ {6, []int{1, 4, 3}},
 	}
 	expectedResult := []int{
 		/* 1 */ 1,
@@ -45,6 +47,7 @@ func TestSeqSum(t *testing.T) {
 	}
 
 	for i, testCase := range testCases {
+		// fmt.Printf("testCase %v\n", i + 1)
 		require.Equal(t, expectedResult[i], SeqSum(testCase.target, testCase.seq), "test number %v\ntarget %v\nseq %v\n", i+1, testCase.target, testCase.seq)
 	}
 }
