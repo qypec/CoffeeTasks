95d8c25283f80a60b0badcacdc657c2f137acc53
qypec <qypec.prog@yandex.ru>
2020-05-22 10:33:24 +0300

    seq_sum | rewrited SeqSum to reader/writer, rewrited tests

--- seq_sum/main.go
+++ seq_sum/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 )
@@ -10,41 +11,24 @@ import (
 const inputFileName = "input.txt"
 const outputFileName = "output.txt"
 
-/* finds the first element that is larger or equal than the given(toFind) */
-/* array a must be sorted */
-/* log(n) */
-func upperBound(a []int, toFind int, l, r int) int {
-	fix := -1
-	for l <= r {
-		m := l + int((r-l)/2)
-		if a[m] >= toFind {
-			fix = m
-			r = m - 1
-		} else {
-			l = m + 1
-		}
-	}
-	return fix
-}
-
-/* BinarySearch finds the first element that is equal than the given (toFind) */
-/* array a must be sorted */
-/* Complexity: log(n) */
-func binarySearch(a []int, toFind int, l, r int) int {
-	fix := -1
-	for l <= r {
-		m := l + int((r-l)/2)
-		if a[m] >= toFind {
-			if a[m] == toFind {
-				fix = m
-			}
-			r = m - 1
-		} else {
-			l = m + 1
-		}
-	}
-	return fix
-}
+// /* BinarySearch finds the first element that is equal than the given (toFind) */
+// /* array a must be sorted */
+// /* Complexity: log(n) */
+// func binarySearch(a []int, toFind int, l, r int) int {
+// 	fix := -1
+// 	for l <= r {
+// 		m := l + int((r-l)/2)
+// 		if a[m] >= toFind {
+// 			if a[m] == toFind {
+// 				fix = m
+// 			}
+// 			r = m - 1
+// 		} else {
+// 			l = m + 1
+// 		}
+// 	}
+// 	return fix
+// }
 
 // my implementation of quick sort (github.com/qypec/basic-algorithms/blob/master/quick_sort/)
 func partition(arr []int, l, r int) int {
@@ -74,40 +58,10 @@ func quickSort(arr []int, l, r int) {
 	quickSort(arr, m+1, r)
 }
 
-// SeqSum searches in a seq for two numbers that in total give a target.
-// If successful, it will return 1, otherwise it will return 0
-// Complexity: nlog(n) + n
-// tests -> github.com/qypec/coffee-tasks/tree/master/seq_sum
-func SeqSum(target int, seq []int) int {
-	quickSort(seq, 0, len(seq)-1) // nlog(n)
-
-	// lower := upperBound(seq, target-seq[len(seq)-1], 0, len(seq)-1) // Removes small numbers
-	lower := 0
-	upper := len(seq) - 1
-	for lower >= 0 && lower < upper {
-		sum := seq[lower] + seq[upper]
-		if sum == target {
-			return 1
-		} else if sum < target {
-			lower++
-		} else {
-			upper--
-		}
-	}
-	return 0
-}
-
-func main() {
-	// openning input file
-	input, err := os.Open(inputFileName)
-	if err != nil {
-		panic(err)
-	}
-	defer input.Close()
-	scanner := bufio.NewScanner(input)
+func readSequence(in io.Reader) (int, []int) {
+	scanner := bufio.NewScanner(in)
 	scanner.Split(bufio.ScanWords)
 
-	// reading sequence
 	scanner.Scan()
 	target, _ := strconv.Atoi(scanner.Text())
 	seq := make([]int, 0)
@@ -117,14 +71,44 @@ func main() {
 			seq = append(seq, num)
 		}
 	}
+	return target, seq
+}
 
-	res := SeqSum(target, seq)
+// SeqSum searches in a seq for two numbers that in total give a target.
+// If successful, it will write to out 1, otherwise 0
+// tests -> github.com/qypec/coffee-tasks/tree/master/seq_sum
+func SeqSum(in io.Reader, out io.Writer) {
+	target, seq := readSequence(in)
+	quickSort(seq, 0, len(seq)-1) // nlog(n)
 
-	// writing the response to the output file
+	ans := 0
+	lower := 0
+	upper := len(seq) - 1
+	for lower >= 0 && lower < upper {
+		sum := seq[lower] + seq[upper]
+		if sum == target {
+			ans = 1
+			break
+		} else if sum < target {
+			lower++
+		} else {
+			upper--
+		}
+	}
+	fmt.Fprintf(out, "%v", ans)
+}
+
+func main() {
+	input, err := os.Open(inputFileName)
+	if err != nil {
+		panic(err)
+	}
+	defer input.Close()
 	output, err := os.Create(outputFileName)
 	if err != nil {
 		panic(err)
 	}
 	defer output.Close()
-	fmt.Fprintf(output, "%v", res)
+
+	SeqSum(input, output)
 }
--- seq_sum/main_test.go
+++ seq_sum/main_test.go
@@ -1,55 +1,52 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-type seqSumTest struct {
-	target int
-	seq    []int
-}
-
-func TestSeqSum(t *testing.T) {
-	testCases := []seqSumTest{
-		/* 1 */ {5, []int{1, 3, 4}},
-		/* 2 */ {1, []int{1}},
-		/* 3 */ {999999998, []int{999999997, 1}},
-		/* 4 */ {2, []int{1, 1, 1, 1, 1, 1, 1}},
-		/* 5 */ {3, []int{1, 1, 1, 1, 1, 1, 1}},
-		/* 6 */ {0, []int{0, 0}},
-		/* 7 */ {1, []int{1, 0}},
-		/* 8 */ {5, []int{1, 2, 5, 2, 0, 3, 2, 4, 3, 3, 1, 0, 5, 5}},
-		/* 9 */ {99, []int{9, 7, 1, 2, 5, 6, 2, 0, 3, 2, 4, 3, 3, 1, 0, 5, 5}},
-		/* 10 */ {0, []int{0}},
-		/* 11 */ {0, []int{0, 0}},
-		/* 12 */ {0, []int{0, 0, 0}},
-		/* 13 */ {22, []int{1, 3, 4, 12, 6, 3, 7, 10}},
-		/* 14 */ {6, []int{1, 4, 3}},
-		/* 15 */ {22, []int{1, 3, 3, 4, 6, 7, 10, 11, 11, 11, 12, 20}},
+func TestSeqSumBasic(t *testing.T) {
+	testCases := []string{
+		/* 1 */ "5\n1 3 4",
+		/* 2 */ "1\n1",
+		/* 3 */ "999999998\n999999997 1",
+		/* 4 */ "2\n1 1 1 1 1 1 1",
+		/* 5 */ "3\n1 1 1 1 1 1 1 2",
+		/* 6 */ "0\n0 0",
+		/* 7 */ "1\n1 0",
+		/* 8 */ "5\n1 2 5 2 0 3 42 2 4 12 3 3 1 0 5 5 8",
+		/* 9 */ "99\n9 7 1 2 5 6 2 0 3 2 4 3 3 1 0 5 5",
+		/* 10 */ "0\n0",
+		/* 11 */ "0\n0 1 3 0 2",
+		/* 12 */ "22\n1 3 4 12 6 3 7 10 27 12",
+		/* 13 */ "6\n1 4 3",
+		/* 14 */ "25\n1 3 3 4 6 7 10 11 11 11 20",
 	}
-	expectedResult := []int{
-		/* 1 */ 1,
-		/* 2 */ 0,
-		/* 3 */ 1,
-		/* 4 */ 1,
-		/* 5 */ 0,
-		/* 6 */ 1,
-		/* 7 */ 1,
-		/* 8 */ 1,
-		/* 9 */ 0,
-		/* 10 */ 0,
-		/* 11 */ 1,
-		/* 12 */ 1,
-		/* 13 */ 1,
-		/* 14 */ 0,
-		/* 15 */ 1,
+	expectedResult := []string{
+		/* 1 */ "1",
+		/* 2 */ "0",
+		/* 3 */ "1",
+		/* 4 */ "1",
+		/* 5 */ "1",
+		/* 6 */ "1",
+		/* 7 */ "1",
+		/* 8 */ "1",
+		/* 9 */ "0",
+		/* 10 */ "0",
+		/* 11 */ "1",
+		/* 12 */ "1",
+		/* 13 */ "0",
+		/* 14 */ "0",
 	}
 
 	for i, testCase := range testCases {
-		fmt.Printf("testCase %v\n", i+1)
-		require.Equal(t, expectedResult[i], SeqSum(testCase.target, testCase.seq), "test number %v\ntarget %v\nseq %v\n", i+1, testCase.target, testCase.seq)
+		in := strings.NewReader(testCase)
+		out := new(bytes.Buffer)
+		SeqSum(in, out)
+		require.Equal(t, expectedResult[i], out.String(), "test number %v\n", i+1)
 	}
 }
