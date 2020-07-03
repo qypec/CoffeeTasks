b9a2cc39f2f6f7e6eaa529d9c7bc617818437b4c
qypec <qypec.prog@yandex.ru>
2020-07-03 17:46:04 +0300

    added new codewars completed task 'reverse_string'

---
+++ codewars/reverse_string/reverse_string.go
@@ -0,0 +1,14 @@
+// Complete the solution so that it reverses the string passed into it.
+
+package main
+
+func Solution(word string) string {
+	result := make([]rune, len(word))
+	
+	i := len(word) - 1
+	for _, letter := range word {
+	  result[i] = rune(letter)
+	  i--
+	}
+	return (string(result))
+}
