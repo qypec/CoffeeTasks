fe8bc78c6bd7a340b91133f9f410641021d1f506
qypec <qypec.prog@yandex.ru>
2020-07-04 12:38:23 +0300

    sql | added composite primary key to table 'order_product' added foreign keys order_id, product_id

--- sql/online_store/shop_fill.sql
+++ sql/online_store/shop_fill.sql
@@ -1,6 +1,4 @@
 
-INSERT INTO product(`brand_id`, `product_type_id`, `category_id`, price) VALUES(1, 1, 1, 8999);
-
 INSERT INTO category(`name`, `discount`) VALUES('Женская одежда', '5');
 INSERT INTO category(`name`, `discount`) VALUES('Мужская одежда', '0');
 INSERT INTO category(`name`, `discount`) VALUES('Женская обувь', '10');
@@ -14,4 +12,11 @@ INSERT INTO brand(`name`) VALUES("GUESS");
 INSERT INTO product_type(`name`) VALUES("Платье");
 INSERT INTO product_type(`name`) VALUES("Футболка");
 
+INSERT INTO product(`brand_id`, `product_type_id`, `category_id`, price) VALUES(1, 1, 1, 8999);
+INSERT INTO product(`brand_id`, `product_type_id`, `category_id`, price) VALUES(1, 2, 1, 7999);
+INSERT INTO product(`brand_id`, `product_type_id`, `category_id`, price) VALUES(2, 1, 3, 6999);
+
 INSERT INTO `order`(user_name, phone, datetime) VALUES('Василий', '555-55-55', '2016-05-09 14:20');
+
+INSERT INTO order_products(order_id, product_id, count) VALUES(1, 1, 1);
+INSERT INTO order_products(order_id, product_id, count) VALUES(1, 2, 3);
--- sql/online_store/shop_tables.sql
+++ sql/online_store/shop_tables.sql
@@ -1,5 +1,24 @@
+drop database shop;
+create database shop;
+use shop;
 
-drop table product;
+CREATE TABLE IF NOT EXISTS category (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  `discount` TINYINT NOT NULL,
+  `alias_name` VARCHAR(128),
+  PRIMARY KEY(`id`));
+
+CREATE TABLE IF NOT EXISTS brand (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  PRIMARY KEY(`id`));
+
+CREATE TABLE IF NOT EXISTS product_type (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  PRIMARY KEY(`id`));
+
 CREATE TABLE IF NOT EXISTS product (
   `id` INT NOT NULL AUTO_INCREMENT,
   `brand_id` INT NOT NULL,
@@ -11,27 +30,6 @@ CREATE TABLE IF NOT EXISTS product (
   FOREIGN KEY(category_id) REFERENCES category(id) ON DELETE CASCADE ON UPDATE NO ACTION,
   PRIMARY KEY(`id`));
 
-drop table category;
-CREATE TABLE IF NOT EXISTS category (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(128) NOT NULL,
-  `discount` TINYINT NOT NULL,
-  `alias_name` VARCHAR(128),
-  PRIMARY KEY(`id`));
-
-drop table brand;
-CREATE TABLE IF NOT EXISTS brand (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(128) NOT NULL,
-  PRIMARY KEY(`id`));
-
-drop table product_type;
-CREATE TABLE IF NOT EXISTS product_type (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(128) NOT NULL,
-  PRIMARY KEY(`id`));
-
-drop table `order`;
 CREATE TABLE IF NOT EXISTS `order` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(128) NOT NULL,
@@ -39,8 +37,10 @@ CREATE TABLE IF NOT EXISTS `order` (
   `datetime` DATETIME NOT NULL,
   PRIMARY KEY(`id`));
 
-drop table order_products;
 CREATE TABLE IF NOT EXISTS order_products (
   `order_id` INT NOT NULL,
   `product_id` INT NOT NULL,
-  `count` INT NOT NULL);
+  `count` INT NOT NULL,
+  FOREIGN KEY(order_id) REFERENCES `order`(id) ON DELETE CASCADE ON UPDATE NO ACTION,
+  FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE NO ACTION,
+  PRIMARY KEY(`order_id`, `product_id`));
