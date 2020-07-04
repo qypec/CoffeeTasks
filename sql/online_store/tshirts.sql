619d66cad6ffea371ccc3268de3ceb785dadde57
qypec <qypec.prog@yandex.ru>
2020-07-04 17:39:19 +0300

    sql | refactored tshirts.sql

--- sql/online_store/tshirts.sql
+++ sql/online_store/tshirts.sql
@@ -1,4 +1,6 @@
-SELECT product.id, brand.name, product_type.name, category.name, product.price FROM product
+use shop;
+
+SELECT product.id, brand.name as brand_name, product_type.name as product_type, category.name as category, product.price FROM product
     inner join brand on product.brand_id = brand.id
     inner join product_type on product.product_type_id = product_type.id
     inner join category on product.category_id = category.id
