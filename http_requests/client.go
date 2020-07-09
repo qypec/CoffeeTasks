0e4136f0229adc67a0ac9f564ed825dd7add25bb
qypec <qypec.prog@yandex.ru>
2020-07-09 17:29:06 +0300

    http_requests | added first basic testcase

--- http_requests/client.go
+++ http_requests/client.go
@@ -52,8 +52,7 @@ type SearchRequest struct {
 	Offset     int    // Можно учесть после сортировки
 	Query      string // подстрока в 1 из полей
 	OrderField string
-	// -1 по убыванию, 0 как встретилось, 1 по возрастанию
-	OrderBy int
+	OrderBy    int // -1 по убыванию, 0 как встретилось, 1 по возрастанию
 }
 
 type SearchClient struct {
@@ -89,7 +88,7 @@ func (srv *SearchClient) FindUsers(req SearchRequest) (*SearchResponse, error) {
 
 	searcherReq, err := http.NewRequest("GET", srv.URL+"?"+searcherParams.Encode(), nil)
 	searcherReq.Header.Add("AccessToken", srv.AccessToken)
-	
+
 	resp, err := client.Do(searcherReq)
 	if err != nil {
 		if err, ok := err.(net.Error); ok && err.Timeout() {
@@ -112,7 +111,7 @@ func (srv *SearchClient) FindUsers(req SearchRequest) (*SearchResponse, error) {
 			return nil, fmt.Errorf("cant unpack error json: %s", err)
 		}
 		if errResp.Error == "ErrorBadOrderField" {
-			return nil, fmt.Errorf("OrderFeld %s invalid", req.OrderField)
+			return nil, fmt.Errorf("OrderField %s invalid", req.OrderField)
 		}
 		return nil, fmt.Errorf("unknown bad request error: %s", errResp.Error)
 	}
